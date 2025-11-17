d8dc9784c84ddc2fd215ec72635c7c4454595f66
kipak1002 <kipak01@gmail.com>
2025-11-18 00:19:17 +0900

    s

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from 'tailwindcss'
 import autoprefixer from 'autoprefixer'
-import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -14,10 +13,5 @@ export default defineConfig({
         autoprefixer,
       ],
     }
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './'),
-    },
   },
 })
