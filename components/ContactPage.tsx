bebb7b9f6e68af0196a73cd5f338706a7929734a
kipak1002 <kipak01@gmail.com>
2026-03-12 00:43:03 +0900

    s

--- components/ContactPage.tsx
+++ components/ContactPage.tsx
@@ -247,7 +247,7 @@ const ContactPage: React.FC<ContactPageProps> = ({ isAdminMode }) => {
           ) : (
             !isLoadingInfo && contactInfo && (
               <div className="animate-fade-in">
-                <h3 className="text-2xl font-serif font-bold text-gray-900 mb-6">CONTACT US</h3>
+                <h3 className="text-2xl font-serif font-bold text-gray-900 mb-6">Follow</h3>
                 <div className="text-gray-700 whitespace-pre-wrap leading-relaxed font-serif text-lg">
                   <Linkify text={contactInfo} />
                 </div>
